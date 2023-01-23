9d3ed35c816260850df95ccdbbdac24f245864ee
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 11:26:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import streamlit as st
 import numpy as np
 
+
+st.title('Il teorema di Pitagora')
+
+st.latex(r'''
+    ipotenusa = \sqrt{a^2 + b^2}
+    ''')
+
 cateto_1 = st.number_input("Inserisci il valore del primo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
 cateto_2 = st.number_input("Inserisci il valore del secondo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
 
 ipotenusa = np.sqrt(cateto_1**2 + cateto_2**2)
 
-st.latex(r'''
-    ipotenusa = \sqrt{a^2 + b^2}
-    ''')
+
 
 st.text(f"L'ipotenusa é {ipotenusa}")
