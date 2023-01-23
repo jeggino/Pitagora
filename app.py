a09ae336eb7136732fecfc2ed0620434736e95bd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 11:22:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,10 @@ cateto_2 = st.number_input("Inserisci il valore del secondo cateto", min_value=N
 
 ipotenusa = np.sqrt(cateto_1**2 + cateto_2**2)
 
+st.latex(r'''
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    ''')
+
 st.text(f"L'ipotenusa é {ipotenusa}")
