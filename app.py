594b6aa5511390da8c352d1a7a4f0047d2419b96
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 11:20:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import streamlit as st
+import numpy as np
 
 cateto_1 = st.number_input("Inserisci il valore del primo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
 cateto_2 = st.number_input("Inserisci il valore del secondo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
 
-ipotenusa = cateto_1**2 + cateto_2**2
+ipotenusa = np.sqrt(cateto_1**2 + cateto_2**2)
 
 st.text(f"L'ipotenusa é {ipotenusa}")
