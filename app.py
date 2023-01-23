2eb381733e6e15466c91f33a4f61253f8205b7b3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 11:41:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.latex(r'''
 cateto_1 = st.number_input("Inserisci il valore del primo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
 cateto_2 = st.number_input("Inserisci il valore del secondo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
 
-ipotenusa = np.sqrt(cateto_1**2 + cateto_2**2)
+ipotenusa = np.sqrt(cateto_1**2 + cateto_2**2).round(2)
 
 
 
