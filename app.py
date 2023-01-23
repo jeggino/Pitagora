e45c88bc47ecb173dc40ae428d62abec4cf6699a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 12:02:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,16 +10,14 @@ with left:
     st.caption("Quello che modernamente conosciamo come teorema di Pitagora viene solitamente attribuito al filosofo e matematico Pitagora. In realtà il suo enunciato (ma non la sua dimostrazione) era già noto ai Babilonesi. Viene a volte affermato che il teorema di Pitagora fosse noto agli antichi Egizi: Carl Boyer esclude questa ipotesi, basandosi sull'assenza del teorema dai papiri matematici rinvenuti. Era conosciuto anche in Cina e sicuramente in India, come dimostrano molte scritture fra cui lo Yuktibhāṣā e gli Śulbasūtra. Non sono note dimostrazioni del teorema considerate tutt'oggi valide e antecedenti o coeve a Pitagora.")
     st.subheader("Enunciato")
     st.caption("In ogni triangolo rettangolo l'area del quadrato costruito sull'ipotenusa è uguale alla somma delle aree dei quadrati costruiti sui cateti.")
+    st.latex(r'''
+    ipotenusa = \sqrt{a^2 + b^2}
+    ''')
 with right:
     image = Image.open('Pythagoras.jpg')
     st.image(image, caption='Samo, tra il 580 a.C. e il 570 a.C. – Metaponto, 495 a.C. circa')
     
 "---"
-
-st.latex(r'''
-    ipotenusa = \sqrt{a^2 + b^2}
-    ''')
-
 
 
 cateto_1 = st.number_input("Inserisci il valore del primo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
