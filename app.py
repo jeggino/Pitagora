39297c1678b46337cfc96f33ab35afe67fecd405
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 11:18:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 
 cateto_1 = st.number_input("Inserisci il valore del primo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
 cateto_2 = st.number_input("Inserisci il valore del secondo cateto", min_value=None, max_value=None, value=0,label_visibility="visible")
+
+ipotenusa = cateto_1**2 + cateto_2**2
+
+st.text(f"L'ipotenusa é {ipotenusa}")
