a429edd285fc9ed7f5e37a77b08ade6f38ba9e6e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 13:43:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,13 @@ with st.sidebar:
     
 
 if page ==  "Notizie generali":  
+    
+    st.title('IL TEOREMA DI PITAGORA')
+    
     left, right = st.columns([2,1])
 
     with left:
-        st.title('IL TEOREMA DI PITAGORA')
+        
         """Il teorema di Pitagora è un teorema della geometria euclidea che stabilisce una relazione fondamentale tra i lati di un triangolo rettangolo."""
         """Quello che modernamente conosciamo come teorema di Pitagora viene solitamente attribuito al filosofo e matematico Pitagora. In realtà il suo enunciato (ma non la sua dimostrazione) era già noto ai Babilonesi. Viene a volte affermato che il teorema di Pitagora fosse noto agli antichi Egizi: Carl Boyer esclude questa ipotesi, basandosi sull'assenza del teorema dai papiri matematici rinvenuti. Era conosciuto anche in Cina e sicuramente in India, come dimostrano molte scritture fra cui lo Yuktibhāṣā e gli Śulbasūtra. Non sono note dimostrazioni del teorema considerate tutt'oggi valide e antecedenti o coeve a Pitagora."""
         st.subheader("Enunciato")
