c9abfd406406bc685d46e99433181b642bf8b291
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2023-01-23 12:29:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,11 @@ with right:
     data_url = base64.b64encode(contents).decode("utf-8")
     file_.close()
     
+    st.markdown(
+        f'<img src="data:image/gif;base64,{data_url}" alt="cat gif">',
+        unsafe_allow_html=True,
+    )
+    
     
 "---"
 
